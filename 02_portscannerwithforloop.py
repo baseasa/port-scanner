890b21254a8a9157a7b4306b8b2b277122ba0f59
beyza <111017915+baseasa@users.noreply.github.com>
2026-09-23 01:30:21 +0300

    Add files via upload

---
+++ 02_portscannerwithforloop.py
@@ -0,0 +1,27 @@
+import socket
+
+target = "x.x.x.x"
+
+for port in range(1, 75):
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    '''
+    socket.socket(..) creates a new socket : socket is an endpoint for sending or receiving data across a computer network
+    socket.AF_INET is the address family for IPv4
+    socket.SOCK_STREAM is the socket type for TCP
+    '''
+    
+    s.settimeout(1)
+
+    result = s.connect_ex((target, port))
+    '''
+    connect_ex() is a method that attempts to connect to the specified address and port
+    it returns 0 if the connection is successful (port is open), or an error code if the 
+    connection fails (port is closed)    
+    '''
+
+    if result == 0:
+        print(f"port {port} is open yay!")
+    else:
+        print(f"port {port} is closed")
+
+    s.close()     
---
+++ README.md
@@ -0,0 +1,6 @@
+a repo where i learn to build a port scanner in python, step by step
+each file adds one new thing on top of the previous one : starting from the simplest one and moving toward more advanced versions
+
+> no extra setup, just python 3 
+
+reminder! -> unauthorized port scanning is illegal in many contries
